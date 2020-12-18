981c209235c1555fa99ef939988887b0875544c5
Debarun Roy <debarun.dr7.roy@gmail.com>
2020-12-18 20:23:32 +0530

    feat: customised app.js file to take api data

---
+++ app.js
@@ -0,0 +1,28 @@
+window.addEventListener("load",()=>{
+    let long;
+    let lat;
+    const key=`8fd8f71615d8b681bb303609304a0859`;
+    let temperatureDesc=document.querySelector(".temp-desc");
+    let tempdegree= document.querySelector(".degree");
+    let timezone = document.querySelector(".timezone")
+    
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position=>{
+            long=position.coords.longitude;
+            lat=position.coords.latitude;
+            const api=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`      
+            
+            fetch(api)
+              .then(response=>{
+                  return response.json();
+              })
+              .then(data=>{
+                  console.log(data);
+                  const {feels_like}= data.main;
+                  tempdegree.textContent=Math.floor(feels_like-273);
+                });
+        });
+    }
+    
+});
+
